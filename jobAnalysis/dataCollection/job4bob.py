72c1ee71e2ca6a343918dad5e9e911aac7c53081
Olivier Philippe <olivier.philippe@gmail.com>
2018-03-07 15:42:55 +0000

    fixing import for job4bob.py

--- jobAnalysis/dataCollection/job4bob.py
+++ jobAnalysis/dataCollection/job4bob.py
@@ -7,11 +7,29 @@ for input into the classifier Bob. Connect to the mongodb and copy file in folde
 """
 import os
 from shutil import copy2
-from include.logger import logger
-from include.configParser import ConfigParserPerso as configParser
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path('.').absolute().parent))
+
+from common.logger import logger
+from common.configParser import ConfigParserPerso as configParser
+
 from job2db import make_sure_path_exists
 from job2db import get_connection
 
+
+# ## GLOBAL VARIABLES  ###
+# # To set up the variable on prod or dev for config file and level of debugging in the
+# # stream_level
+RUNNING = 'dev'
+
+if RUNNING == 'dev':
+    CONFIG_FILE = '../config/config_dev.ini'
+    DEBUGGING='DEBUG'
+elif RUNNING == 'prod':
+    CONFIG_FILE = '../config/config.ini'
+    DEBUGGING='INFO'
 
 logger = logger(name='jobs4bob', stream_level='DEBUG')
 
@@ -41,31 +59,36 @@ def copy_file(filename, inputfolder, outputfolder):
 def main():
     """
     """
-    # ### Get the variables NAMES from the config.ini file
-    config_value = configParser().read_config('./config.ini')
-    # config_value = configParser().read_config('./config_dev.ini')
+    # set up access credentials
+    config_value = configParser()
+    config_value.read(CONFIG_FILE)
 
     # Get the folder or the file where the input data are stored
-    INPUT_FOLDER = config_value.get('INPUT_FOLDER'.lower(), None)
-    DB_ACC_FILE = config_value.get('DB_ACCESS_FILE'.lower(), None)
+    INPUT_FOLDER = config_value['input'].get('INPUT_FOLDER'.lower(), None)
+    DB_ACC_FILE = config_value['db_access'].get('DB_ACCESS_FILE'.lower(), None)
 
-    sample_value = configParser().read_config('./job4bob_config.ini')
-    SAMPLE_FOLDER = sample_value.get('SAMPLE_OUT_FOLDER'.lower(), None)
+    SAMPLE_FOLDER = config_value['BOB'].get('SAMPLE_OUT_FOLDER'.lower(), None)
     # Check if the folder exists and if not, create it
     make_sure_path_exists(SAMPLE_FOLDER)
 
-    access_value = configParser().read_config(DB_ACC_FILE)
+    access_value = configParser()
+    access_value.read(DB_ACC_FILE)
+    # # MongoDB ACCESS # #
+    mongoDB_USER = access_value['MongoDB'].get('db_username'.lower(), None)
+    mongoDB_PASS = access_value['MongoDB'].get('DB_PASSWORD'.lower(), None)
+    mongoDB_AUTH_DB = access_value['MongoDB'].get('DB_AUTH_DB'.lower(), None)
+    mongoDB_AUTH_METH = access_value['MongoDB'].get('DB_AUTH_METHOD'.lower(), None)
 
-    DB_NAME = access_value.get('DB_NAME'.lower(), None)
-    DB_COLL = access_value.get('DB_COLLECTION'.lower(), None)
-    DB_USER = access_value.get('DB_USERNAME'.lower(), None)
-    DB_PASS = access_value.get('DB_PASSWORD'.lower(), None)
-    DB_AUTH_DB = access_value.get('DB_AUTH_DB'.lower(), None)
-    DB_AUTH_METH = access_value.get('DB_AUTH_METHOD'.lower(), None)
+    # Get the information about the db and the collections
+    mongoDB_NAME = config_value['MongoDB'].get('DB_NAME'.lower(), None)
+    mongoDB_JOB_COLL = config_value['MongoDB'].get('DB_JOB_COLLECTION'.lower(), None)
+
+    # ### Init the processes #####
 
     # Connect to the database
     logger.info('Connection to the database')
-    db_jobs = get_connection(DB_NAME, DB_COLL, DB_USER, DB_PASS, DB_AUTH_DB, DB_AUTH_METH)
+    db_jobs = get_connection(mongoDB_NAME, mongoDB_JOB_COLL, mongoDB_USER,
+                             mongoDB_PASS, mongoDB_AUTH_DB, mongoDB_AUTH_METH)
 
     for filename in get_valid_id(db_jobs):
         print(filename)
