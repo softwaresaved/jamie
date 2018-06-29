ccc8ccf912976c00c8dbeb225b8c6e2b72a0ee67
Olivier Philippe <olivier.philippe@gmail.com>
2018-06-29 14:58:19 +0100

    Data - Get the list of jobs which were classified during the second run

--- jobAnalysis/report/analyse_second_run_classification.py
+++ jobAnalysis/report/analyse_second_run_classification.py
@@ -8,6 +8,7 @@ Collect the data and analyse the quality of the classifier after the second run
 import os
 import csv
 import argparse
+from shutil import copy2
 from datetime import timedelta
 
 from bson import Code
@@ -38,10 +39,11 @@ if __name__ == "__main__":
     # Connect to the database
     db_conn = connectDB(CONFIG_FILE)
 
-    filename = '../../outputs/modelCreation/second_run_classification.csv'
+    file_tags = '../../outputs/modelCreation/second_run_classification.csv'
+    file_info = '../../outputs/modelCreation/second_run_info.csv'
     list_tags_to_match = []
 
-    with open(filename, 'w') as f:
+    with open(file_tags, 'w') as f:
         writer = csv.DictWriter(f, fieldnames=['jobid', 'SoftwareJob', 'tags', 'source'])
         writer.writeheader()
         for doc in db_conn.second_run.find():
@@ -59,6 +61,7 @@ if __name__ == "__main__":
                                     'source': 'first_batch'}
             writer.writerow(row)
 
+        set_keys_of_all_jobs = set()
         for jobid in list_tags_to_match:
             doc = db_conn.jobs.find_one({'jobid': jobid})
             if doc:
@@ -66,4 +69,13 @@ if __name__ == "__main__":
                        'SoftwareJob': doc['prediction'],
                        'source': 'machine_learning'}
                 writer.writerow(row)
+                set_keys_of_all_jobs.update(doc.keys())
+
+    with open(file_info, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=set_keys_of_all_jobs)
+        writer.writeheader()
+        for jobid in list_tags_to_match:
+            doc = db_conn.jobs.find_one({'jobid': jobid})
+            if doc:
+                writer.writerow(doc)
 
--- jobAnalysis/report/get_jobs_per_checking.py
+++ jobAnalysis/report/get_jobs_per_checking.py
@@ -7,7 +7,6 @@ import sys
 import random
 from pathlib import Path
 from shutil import copy2
-from io import StringIO
 
 import pandas as pd
 import numpy as np
