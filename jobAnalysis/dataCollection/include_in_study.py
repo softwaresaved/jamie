9005760ea4c476eeab3e7dae71c75051fa220f86
Olivier Philippe <olivier.philippe@gmail.com>
2018-12-06 10:54:14 +0000

    Update: finishing script to create the key: inclde_in_study

--- jobAnalysis/dataCollection/include_in_study.py
+++ jobAnalysis/dataCollection/include_in_study.py
@@ -44,7 +44,18 @@ logger = logger(name="include_in_study", stream_level="DEBUG")
 
 # TODO, move this list of requirements into the config file when reworked
 
-requirements = {}
+requirements = {'extra_location': {'$in' : ["Northern England",
+                                            "London",
+                                             "Midlands of England",
+                                             "Scotland",
+                                             "South West England",
+                                             "South East England",
+                                             "Wales",
+                                             "Republic of Ireland",
+                                             "Northern Ireland"]},
+                'placed_on': {'$exists': True},
+                'prediction': {'$ne': 'None'},
+                'not_student': True}
 
 
 class IncludeInStudy:
@@ -73,7 +84,7 @@ class IncludeInStudy:
         """
         self.requirements = requirements
         self.db = db
-        self.jobid = jobid
+        self.redo = redo
         # A counter for all the documents parsed
         self.count_all_doc = 0
         # A counter for all the documents with to_include = True
@@ -91,7 +102,7 @@ class IncludeInStudy:
             if self.redo = False
         """
         if self.redo is False:
-            return {'include_in_study' {'$exists': False}}
+            return {'include_in_study': {'$exists': False}}
         else:
             return {}
 
@@ -106,9 +117,114 @@ class IncludeInStudy:
             : gen() MongoDB_document(): all the matching documents
         """
         for doc in self.db.find(to_select):
+            self.count_all_doc +=1  # inc the global count of all docs
             yield doc
 
-    def
+    def _check_inclusion(self, document):
+        """
+        Check if the document passed meet the requirements and return True if it does
+        and False if it doesnt
+
+        :params:
+            :document dict(): job record itself as found in the db
+
+        :return:
+            :bool(): True if it meets the requirements, False if it does not
+        """
+        for key in self.requirements:
+            try:
+                type_check = self.requirements[key]
+                if isinstance(type_check, dict):
+                    type_check = list(type_check.keys())[0]
+                if type_check == '$exists':
+                    if str(self.requirements[key]) == 'True':
+                        if key not in document:
+                            raise KeyError
+                    elif str(self.requirements[key]) == 'False':
+                        if key in document:
+                            raise KeyError
+
+                elif type_check == '$ne':
+                    if str(document[key]) == str(self.requirements[key][type_check]):
+                        raise KeyError
+
+                elif type_check == '$eq':
+                    if str(document[key]) != str(self.requirements[key][type_check]):
+                        raise KeyError
+
+                elif type_check == '$in':
+                    if isinstance(document[key], list):
+                        for item in document[key]:
+                            if item not in self.requirements[key][type_check]:
+                                raise KeyError
+                    else:
+                        if document[key] not in self.requirements[key][type_check]:
+                            raise KeyError
+
+                elif type_check == '$nin':
+                    if isinstance(document[key], list):
+                        for item in document[key]:
+                            if document[key] in self.requirements[key][type_check]:
+                                raise KeyError
+                    else:
+                        print(self.requirements[key][type_check])
+                        if document[key] in self.requirements[key][type_check]:
+                            raise KeyError
+
+                else:
+                    if str(document[key]) != str(self.requirements[key]):
+                        raise KeyError
+
+            except KeyError:   # In case the key is not present it is directly not include
+                self.count_not_to_include +=1
+                return False
+            except TypeError:
+                print(document)
+                raise
+        self.count_to_include +=1
+        return True
+
+    def _get_id(self, document):
+        """
+        Get the jobid from the document
+
+        :params:
+            :document dict(): representing the record
+
+        :return:
+            :str(): the jobid of the document
+        """
+        return document['jobid']
+
+    def _update_record(self, document, include_in_study):
+        """
+        Update the record with the value_include in include_in_stud
+
+        :params:
+            :document dict(): record from the db
+            :include_in_study bool: value to insert in the key include_in_study
+
+        :return:
+            :None: update directly the matching document in the db if found with the
+            jobid str() key.
+        """
+        jobid = self._get_id(document)
+        # self._output_count()
+        self.db.update_one({'jobid': jobid},
+                           {'$set': {'include_in_study': include_in_study}},
+                           upsert=False)
+
+    def _output_count(self):
+        """
+        print the different counts set in __init__
+        :params:
+            :None: use the self.counts
+        :return:
+            :print: results of the self.counts
+        """
+        print('Total documents parsed: {}'.format(self.count_all_doc))
+        print('\tDocuments to include in the study: {}'.format(self.count_to_include))
+        print('\tDocument Not included in study: {}'.format(self.count_not_to_include))
 
     def run(self):
         """
@@ -116,9 +232,11 @@ class IncludeInStudy:
         """
         to_select = self._select_records()
 
-        for document in self.retrieve_doc(to_select):
+        for document in self._retrieve_doc(to_select):
             value_include = self._check_inclusion(document)
+            self._update_record(document, value_include)
 
+        self._output_count()
 
 
 
@@ -145,10 +263,10 @@ def main():
     # Connect to the database
     logger.info("Connection to the database")
     db_jobs = db_conn["jobs"]
-    to_include = IncludeInStudy(db, requirements, redo=True)
+    to_include = IncludeInStudy(db_jobs, requirements, redo=True)
     to_include.run()
 
-if __main__():
+if __name__ == "__main__":
     main()
 
 
