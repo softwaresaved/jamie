d284347231adce0e0da06e1d4b3741a6140d85f1
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-04-22 23:04:43 +0400

    Use new config

--- jamie/common/getConnection.py
+++ jamie/common/getConnection.py
@@ -2,18 +2,19 @@
 # encoding: utf-8
 
 import pymongo
-# import pymysql
 import mysql.connector
+import pytoml as toml
 
 
-from common.getArgs import getArgs
-from common.configParser import configParserPerso as configParser
-from common.logger import logger
+from ..logger import logger
 
 logger = logger(name='getConnection', stream_level='DEBUG')
 
+def read_toml(fn):
+    with open(fn) as fp:
+        return toml.load(fp)
 
-def connectMongo(config):
+def connectMongo(cfg):
     """
     """
     def connectDB(*args):
@@ -24,35 +25,23 @@ def connectMongo(config):
         db = args[0]
         c = pymongo.MongoClient()
         try:
-            user = args[1]
-            passw = args[2]
-            db_auth = args[3]
-            db_mech = args[4]
-            confirmation = c[db].authenticate(user,
-                                                passw,
-                                                source=db_auth,
-                                                mechanism=db_mech)
+            user, passwd, db_auth, db_mech = args
+            confirmation = c[db].authenticate(user, passwd, source=db_auth, mechanism=db_mech)
             logger.info('Authenticated: {}'.format(confirmation))
         except (IndexError, ValueError, TypeError):
             logger.info('Connection to the database without password and authentication')
         return c[db]
 
-    # set up access credentials
-    config_value = configParser()
-    args_to_connect = list()
-    args_to_connect.append(config.DB_NAME)
-    # # MongoDB ACCESS # #
-    try:
-        DB_ACC_FILE = config.DB_ACCESS_FILE
-        access_value = configParser()
-        access_value.read(DB_ACC_FILE)
-        args_to_connect.append(access_value['MongoDB'].get('db_username', None))
-        args_to_connect.append(access_value['MongoDB'].get('DB_PASSWORD', None))
-
-        args_to_connect.append(access_value['MongoDB'].get('DB_AUTH_DB', None))
-        args_to_connect.append(access_value['MongoDB'].get('DB_AUTH_METHOD', None))
-    except KeyError:
-        pass
+    args_to_connect = [cfg['db.name']]
+    # # MongoDB ACCESS #
+    if 'db.access' in cfg:
+        access_value = read_toml(cfg['db.access'])
+        args_to_connect += [
+            access_value['MongoDB'].get('db_username', None),
+            access_value['MongoDB'].get('DB_PASSWORD', None),
+            access_value['MongoDB'].get('DB_AUTH_DB', None),
+            access_value['MongoDB'].get('DB_AUTH_METHOD', None)
+        ]
 
     # Get the information about the db and the collections
     # Create the instance that connect to the db storing the training set
@@ -60,7 +49,7 @@ def connectMongo(config):
     return mongoDB
 
 
-def connectMysql(config):
+def connectMysql(cfg):
     """
     Connection to MySQL using MySQLdb module
     and return a cursor
@@ -74,26 +63,16 @@ def connectMysql(config):
         # return connector.cursor(mdb.cursors.DictCursor)
 
     # set up access credentials
-    args_to_connect = list()
-    kwargs_to_connect = dict()
-    args_to_connect.append(config.MYSQL_db_host)
-    kwargs_to_connect['host'] = config.MYSQL_db_host
-    args_to_connect.append(config.MYSQL_db_name)
-    kwargs_to_connect['db'] = config.MYSQL_db_name
-    args_to_connect.append(config.MYSQL_port)
-    kwargs_to_connect['port'] = config.MYSQL_port
-    try:
-        DB_ACC_FILE = config.DB_ACCESS_FILE
-        access_value = configParser()
-        access_value.read(DB_ACC_FILE)
-
-        # # MYSQL ACCESS # #
-        args_to_connect.append(access_value['MYSQL'].get('db_username', None))
-        kwargs_to_connect['user'] = access_value['MYSQL'].get('db_username', None)
-        args_to_connect.append(access_value['MYSQL'].get('db_password', None))
-        kwargs_to_connect['passwd'] = access_value['MYSQL'].get('db_password', None)
-
-    except KeyError:
-        pass
+    kwargs_to_connect = {
+        'host': cfg['db.mysql-host'],
+        'db': cfg['db.mysql-name'],
+        'port': cfg['db.mysql-port']
+    }
+    if 'db.access' in cfg:
+        access_value = read_toml(cfg['db.access'])
+        kwargs_to_connect.update({
+            'user': access_value['MYSQL'].get('db_username', None),
+            'passwd': access_value['MYSQL'].get('db_password', None)
+        })
 
     return connectDB(**kwargs_to_connect)
