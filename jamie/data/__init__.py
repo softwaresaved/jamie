caded3ba84220d44daf187b2dee4e344f32cc313
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-04-26 17:29:20 +0100

    Support multiple employer lists

--- jamie/data/__init__.py
+++ jamie/data/__init__.py
@@ -1,1 +1,17 @@
-from .importer import main as importer
+from pathlib import Path
+import pandas as pd
+from box import Box
+from .importer import main as importer  # NOQA
+
+def read_list(fn):
+    lines = Path(fn).read_text().split('\n')
+    return [l.strip() for l in lines if l.strip()]
+
+
+# Datasets
+datasets = Box({
+    'uk_uni': {
+        'list': read_list(Path(__file__).parent / 'uk_uni_list.txt'),
+        'postcodes': pd.read_csv(Path(__file__).parent / 'uk_uni_postcode.csv')
+    }
+})
