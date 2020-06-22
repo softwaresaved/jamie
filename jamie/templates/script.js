2a369724d29779c05f998e21f89944aba6dc3ad5
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-06-22 11:38:39 +0100

    Add metricsgraphics graphs

---
+++ jamie/templates/script.js
@@ -0,0 +1,59 @@
+d3.json('by_year.json', function(data) {
+    MG.data_graphic({
+        title: "Number of jobs by year",
+        data: data,
+        width: 450,
+        height: 250,
+        area: false,
+        color: "#2155a8",
+        target: '#njobsyear',
+        x_accessor: 'group',
+        y_accessor: 'npos',
+        brush: 'x'
+    })
+})
+d3.json('by_year.json', function(data) {
+    MG.data_graphic({
+        title: "Proportion of jobs by year",
+        data: data,
+        width: 450,
+        height: 250,
+        area: false,
+        color: "#2155a8",
+        target: '#propjobsyear',
+        x_accessor: 'group',
+        y_accessor: 'proportion_pos',
+        brush: 'x'
+    })
+})
+d3.json('by_month.json', function(data) {
+    data = MG.convert.date(data, 'group');
+    MG.data_graphic({
+        title: "Number of jobs by month",
+        data: data,
+        width: 450,
+        height: 250,
+        area: false,
+        color: "#2155a8",
+        target: '#njobsmonth',
+        x_accessor: 'group',
+        y_accessor: 'npos',
+        brush: 'x'
+    })
+})
+d3.json('by_month.json', function(data) {
+    data = MG.convert.date(data, 'group');
+    MG.data_graphic({
+        title: "Proportion of jobs by month",
+        data: data,
+        width: 450,
+        height: 250,
+        area: false,
+        color: "#2155a8",
+        target: '#propjobsmonth',
+        x_accessor: 'group',
+        y_accessor: 'proportion_pos',
+        brush: 'x'
+    })
+})
+
