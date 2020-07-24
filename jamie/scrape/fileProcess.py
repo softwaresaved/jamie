606769705c8b23b1235e1e5715d6319c87911d20
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-07-24 19:48:51 +0100

    Revamp importer first draft
    Working on revamping the importer. This commit starts cleaning
    fileProcess.py and simplifying details. Rather than have a class with
    very little shared code (in most cases, the `self` attribute was not
    being used at all), moving to a JobFile class to represent attributes of
    a single parsed jobs.ac.uk HTML file.

--- jamie/scrape/fileProcess.py
+++ jamie/scrape/fileProcess.py
@@ -6,29 +6,54 @@ import json
 import itertools
 import bs4
 import string
+from pathlib import Path
+from typing import Union
 
 
-class fileProcess(object):
+_table_punc = bytes.maketrans(str.encode(string.punctuation), b' ' * \  # NOQA
+                              len(string.punctuation))
+_table_space = bytes.maketrans(bytes(' ', 'utf-8'), bytes('_', 'utf-8'))
+
+class JobFile:
     """
-    To get access to files
+    Represents a single jobs.ac.uk scraped document
+
+    Parameters
+    ----------
+    content : Path or str
+        If type Path, filename of jobs.ac.uk scraped document, or if str,
+        the content itself
+
+    Attributes
+    ----------
+    data : dict
+        Parsed data from HTML content
     """
 
-    def __init__(self, root_folder=None):
-        """
-        """
-        self.table_punc = bytes.maketrans(str.encode(string.punctuation), b' '* len(string.punctuation))
-        self.table_space = bytes.maketrans(bytes(' ', 'utf-8'), bytes('_', 'utf-8'))
-        self.root_folder = root_folder
+    _matching_key = ['jobid', 'name', 'employer', 'location', 'salary',
+                     'hours', 'contract', 'placed_on', 'closes',
+                     'description', 'extra_type_role',
+                     'extra_subject_area', 'extra_location']
+    data = dict()
 
-        self.matching_key = ['jobid', 'name', 'employer', 'location', 'salary',
-                             'hours', 'contract', 'placed_on', 'closes',
-                             'description',
-                             'extra_type_role', 'extra_subject_area',
-                             'extra_location']
+    def __init__(self, content: Union[Path, str]):
+        if isinstance(content, Path):
+            self.filename = content
+            self._content = self.filename.read_text()
+        elif isinstance(content, str):
+            self._content = content
+        else:
+            raise ValueError("content must be one of Path or str")
+        self._soup = bs4.BeautifulSoup(self._content, 'html.parser')
 
+        # Enhanced content alters behaviour of some parsing
+        if soup.find('div', {'id': 'enhanced-content'}):
+            self.enhanced = True
+        else:
+            self.enhanced = False
+
+    @staticmethod
     def transform_key(self, key_string):
-        """
-        """
         key_string = key_string.lower()
         key_string = str.encode(key_string, 'utf-8').translate(self.table_punc)
         key_string = key_string.translate(self.table_space)
@@ -38,85 +63,64 @@ class fileProcess(object):
         key_string = key_string.replace(' ', '_')
         return key_string
 
-    def get_raw_content(self, file_name):
-        """
-        Parse the filename and output an object of the content
-        """
-        with open('{}/{}'.format(self.root_folder, file_name), 'r') as f:
-            return f.read()
-
-    def process_result(self, dict_, result, key):
-        """
-        """
-        if result:
-            dict_[key] = result
-        return dict_
-
-    def get_employer(self, soup, enhanced):
-        """
-        """
-        employer = None
+    @property
+    def employer(self):
+        _employer = None
         try:
-            employer = soup.find('h3').text
+            _employer = self._soup.find('h3').text
         except AttributeError:
             try:
-                for emp in soup.find('a').get('href'):
+                for emp in self._soup.find('a').get('href'):
                     if emp[:10] == '/employer/':
-                        employer = emp[:10]
+                        _employer = emp[:10]
             except (AttributeError, TypeError):
                 pass
-        return employer
+        return _employer
 
-    def get_job_title(self, soup, enhanced):
-        """
-        """
+    @property
+    def job_title(self):
         try:
-            return soup.find('h1').text
+            return self._soup.find('h1').text
         except AttributeError:
             return None
 
-    def get_place(self, soup, enhanced):
-        """
-        """
+    @property
+    def place(self):
         try:
-            return soup.find('h3').text
+            return self._soup.find('h3').text
         except AttributeError:
             return None
 
-    def get_details(self, soup, enhanced):
-        """
-        """
+    def details(self):
         table_to_match = 'td'
         class_to_match = 'detail-heading'
 
-        for element in soup.findAll(table_to_match, {'class': class_to_match}):
+        for element in self._soup.findAll(table_to_match, {'class': class_to_match}):
             key = element.text
             key = self.transform_key(key)
             content = element.findNext('td')
             content = content.text
             yield {key: content}
 
-    def get_description(self, soup, enhanced):
-        """
-        Get the description, aka main text from the file
-        """
+    @property
+    def description(self, soup, enhanced):
         # To add space when encounter <p> and <br> tags otherwise words are
         # attached
         # Some ads have the first div as <div id='enhanced-content'> which
         # change the structure of the html
-        if enhanced == 'enhanced':
+        if self.enhanced:
             try:
-                section = soup.findAll('div', {'class': 'section', 'id': None})[0]
+                section = self._soup.findAll('div', {'class': 'section', 'id': None})[0]
                 return section.get_text(separator=u' ')
             except IndexError:
                 pass
             try:
-                section = soup.findAll('div', {'id': 'enhanced-right'})[0]
+                section = self._soup.findAll('div', {'id': 'enhanced-right'})[0]
                 return section.get_text(separator=u' ')
             except IndexError:
                 pass
             try:
-                section = soup.findAll('div', {'id': 'enhanced-content'})[0]
+                section = self._soup.findAll('div', {'id': 'enhanced-content'})[0]
                 return section.get_text(separator=u' ')
             except IndexError:
                 print(soup)
@@ -124,13 +128,19 @@ class fileProcess(object):
 
         else:
             try:
-                section = soup.findAll('div', {'class': 'section', 'id': None})[1]
+                section = self._soup.findAll('div', {'class': 'section', 'id': None})[1]
                 return section.get_text(separator=u' ')
             except IndexError:
                 pass
             try:
+                section = self._soup.findAll('div', {'id': 'job-description'})[1]
+                return section.get_text(separator=u' ')
+            except IndexError:
+                pass
+ 
+            try:
                 description_text = []
-                section = soup.find('div', {'class': 'col-lg-12'})
+                section = self._soup.find('div', {'class': 'col-lg-12'})
                 # Need to find the first <p>. The description is under that one
                 # but also contains differents tags
                 text_desc = False
@@ -143,82 +153,53 @@ class fileProcess(object):
             except AttributeError:
                 pass
 
-    def get_extra_details(self, soup, enhanced):
+    def extra_details(self):
         """
         Get the extra details at the end of description
         """
-
-        for section in soup.findAll('div', {'class': 'inlineBox'}):
-            key = section.find('p')
-            if key:
-                original_content = key.findNext('p')
-                key = self.transform_key(key.text)
-                result = list()
-                for element in original_content.findAll('a'):
-                    result.append(element.text)
-                # Sometime the content is not within a tag <p> and
-                # within <a> tag but under a
-                # <div class='j-nav-pill-box'> tags
-                # Check if the previous one give results and if not
-                # try to parse the <div> tag
-                # Work for <p>Subject Area(s) don't know for the others
-                if len(result) == 0:
-                    second_content = section.findNext('div')
-                    for element in second_content.findAll('a'):
+        if not self.enhanced:
+            for section in self._soup.findAll('div', {'class': 'inlineBox'}):
+                key = section.find('p')
+                if key:
+                    original_content = key.findNext('p')
+                    key = self.transform_key(key.text)
+                    result = list()
+                    for element in original_content.findAll('a'):
                         result.append(element.text)
+                    # Sometime the content is not within a tag <p> and
+                    # within <a> tag but under a
+                    # <div class='j-nav-pill-box'> tags
+                    # Check if the previous one give results and if not
+                    # try to parse the <div> tag
+                    # Work for <p>Subject Area(s) don't know for the others
                     if len(result) == 0:
-                        result = original_content.text
-                yield {'extra_{}'.format(key): result}
-
-
-    def get_extra_details_enhanced(self, soup):
-        """
-        """
-        for element in soup.findAll('td', {'class': 'detail-heading'}):
-            key = element.text
-            key = self.transform_key(key)
-            content = element.findNext('td')
-            content = content.text
-            yield {key: content}
-
-    def parse_html(self, dict_output, soup):
-        """
-        """
-        if soup.find('div', {'id': 'enhanced-content'}):
-            enhanced = 'enhanced'
+                        second_content = section.findNext('div')
+                        for element in second_content.findAll('a'):
+                            result.append(element.text)
+                        if len(result) == 0:
+                            result = original_content.text
+                    yield {'extra_{}'.format(key): result}
         else:
-            enhanced = 'normal'
+            for element in self._soup.findAll('td', {'class': 'detail-heading'}):
+                key = element.text
+                key = self.transform_key(key)
+                content = element.findNext('td')
+                content = content.text
+                yield {key: content}
 
-        dict_output['enhanced'] = enhanced
+    def parse(self):
+        self.data = {
+            'description': self.description,
+            'employer': self.employer,
+            'name': self.job_title,
+            'location': self.place,
+        }
+        for d in self.details():
+            self.data.update(d)
 
-        key = 'description'
-        result = self.get_description(soup, enhanced)
-        dict_output = self.process_result(dict_output, result, key)
-
-        key = 'employer'
-        result = self.get_employer(soup, enhanced)
-        dict_output = self.process_result(dict_output, result, key)
-
-        key = 'name'
-        result = self.get_job_title(soup, enhanced)
-        dict_output = self.process_result(dict_output, result, key)
-
-        key = 'location'
-        result = self.get_place(soup, enhanced)
-        dict_output = self.process_result(dict_output, result, key)
-
-        for details in self.get_details(soup, enhanced):
-            dict_output.update(details)
-
-
-        # Only present if not enhanced-content
-        if enhanced is True:
-            for extra_details in self.get_extra_details_enhanced(soup):
-                dict_output.update(extra_details)
-        else:
-            for extra_details in self.get_extra_details(soup, enhanced):
-                dict_output.update(extra_details)
-        return dict_output
+        for extra_details in self.extra_details():
+            self.data.update(extra_details)
+        return self.data
 
     def _extract_json_ads(self, data):
         """
