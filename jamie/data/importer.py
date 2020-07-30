70813e0bf96a41273ca09645fa92c1b92e5624b7
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-07-30 16:39:57 +0000

    importer: Remove one-liner functions, general code cleanup

--- jamie/data/importer.py
+++ jamie/data/importer.py
@@ -6,13 +6,10 @@ Python module to import scraped job data in HTML format
 downloaded from www.jobs.ac.uk to mongodb, after cleaning.
 """
 
-import os
 import sys
-import itertools
 import pymongo
 from collections import defaultdict
 from ..logger import logger
-from ..config import Config
 from ..common.getConnection import connectMongo
 from ..scrape.fileProcess import JobFile
 from . import valid_employer
@@ -20,44 +17,26 @@ from . import valid_employer
 logger = logger(name="importer", stream_level="DEBUG")
 REPORT_INTERVAL = 1000  # report progress of database import every N jobs
 
-def get_filename(root_folder, *args):
-    """
-    Return the name of the file, used as JobId in later processes
-    """
-    for dirname, subdir, files in os.walk(root_folder):
-        for file_ in files:
-            if file_ not in itertools.chain(*args):
-                yield file_
+def _import_iterator(input_folder, skip):
+    """Iterate over JobFile data
 
-def data_from_file(input_folder, infiles):
+    Parameters
+    ----------
+    input_folder : Path
+        Input folder containing job data in HTML format
+    skip : List[str]
+        List of file stems (without suffix) to skip. Usually this is
+        the set of already recorded jobids
+
+    Yields
+    ------
+    dict
+        Job data in a dictionary
     """
-    Function to get data from the different files within a folder
-    and yield a dictionary containing the parsed information
-    """
-    for filename in infiles:
+    for filename in (f for f in input_folder.glob("*") if f.stem not in skip):
         job = JobFile(input_folder / filename).parse()
         if job:
             yield job
-
-def create_index(coll, key, unique=False):
-    """
-    Check if index exists and if not, creates it.
-    MongoDB does not recreate it if already existing
-    :params:
-        :coll: pymongo collection object
-        :key: str() the key for the index
-        :unique: bool to set up the key as unique or not
-    """
-    coll.create_index(key, unique=unique)
-
-
-def get_db_ids(db):
-    """
-    Connect to the db and parse all the documents
-    return a list with the jobIds
-    """
-    return db.distinct("jobid")
-
 
 def main(config, employer='uk_uni'):
     """Import data from HTML to MongoDB
@@ -77,16 +56,12 @@ def main(config, employer='uk_uni'):
 
     db_conn = connectMongo(config)
     db_jobs = db_conn[config['db.jobs']]
-    create_index(db_jobs, "jobid", unique=True)  # faster searches for "jobid"
+    db_jobs.create_index("jobid", unique=True)  # faster searches for "jobid"
 
-    logger.info("Collecting the already recorded jobs")
-    recorded_jobs_list = get_db_ids(db_jobs)
-    logger.info("Already recorded jobs: {}".format(len(recorded_jobs_list)))
-
-    # new_jobs_list has only the jobs not in recorded_jobs_list
-    new_jobs_list = get_filename(config['scrape.folder'], recorded_jobs_list)
+    recorded_jobs = db_jobs.distinct("jobid")
+    logger.info("Already recorded jobs: {}".format(len(recorded_jobs)))
     njobs = defaultdict(int)
-    for data in data_from_file(config['scrape.folder'], new_jobs_list):
+    for data in _import_iterator(config['scrape.folder'], skip=recorded_jobs):
         if njobs['inserted'] % REPORT_INTERVAL == 0:
             logger.debug("Progress %s", njobs)
         try:
