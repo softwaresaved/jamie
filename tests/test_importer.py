905452a3fe1cccf43df93bd63ec08ce13a0e5a59
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-07-30 17:22:25 +0000

    tests: Add get_nested_key() in importer

---
+++ tests/test_importer.py
@@ -0,0 +1,31 @@
+import pytest
+from jamie.scrape.fileProcess import get_nested_key
+
+d = {
+    'json': {
+        'datePosted': '2020-04-12',
+        'hiringOrganization': {
+            'department': {
+                'name': 'computer_science'
+            }
+        },
+        'jobLocation': {
+            'address': {
+                'addressRegion': 'England'
+            }
+        },
+        'description': 'Another job description here',
+    },
+    'base': True
+}
+
+@pytest.mark.parametrize(
+    "key,value",
+    [
+        ("json.datePosted", "2020-04-12"),
+        ("json.hiringOrganization.department.name", "computer_science"),
+        ("json.jobLocation.address.addressRegion", "England"),
+        ("json.description", "Another job description here"),
+    ])
+def test_get_nested_key(key, value):
+    assert get_nested_key(d, key) == value
