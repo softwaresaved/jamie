88a2da8086bdbb89d486a877fab48f241e2d6949
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-08-07 22:58:57 +0000

    Remove unused jamie.common module

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="",
     author_email="hello@example.com",
     scripts=["bin/jamie"],
-    packages=["jamie", "jamie.common", "jamie.scrape", "jamie.features", "jamie.data"],
+    packages=["jamie", "jamie.scrape", "jamie.features", "jamie.data"],
     install_requires=[
         "chevron",  # mustache templating
         "matplotlib",
